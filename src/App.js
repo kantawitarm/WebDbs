e25cb263957a10296df8ce2d23e6bd04e35943d6
Arm_com <kantawitsakhet@gmail.com>
2021-09-29 20:48:50 +0700

    Add Search box to nav bar

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import MyNavbar from './components/MyNavbar';
 //Arm section
 function App() {
   return (
-    <div className="App">
+    <div style={{textAlign: "center"}} className="Home-Back"> {/* ต้อง กำหนดค่า กึ่งกลางที่ตัวใหญ่ ตัวคลุม */}
     <MyNavbar/>
     <Button variant="primary">Button #1</Button>
 
